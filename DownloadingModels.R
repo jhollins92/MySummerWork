7660a122522fca11c80e830d4dc22537163dfc0c
Colin Gillespie <csgillespie@gmail.com>
2012-07-17 10:38:29 +0100

    Can now download directly from biomodels

--- DownloadingModels.R
+++ DownloadingModels.R
@@ -1,7 +1,16 @@
-rm(list=ls())
 library(RCurl)
 library(httr)
-download.file("ftp://ftp.ebi.ac.uk/pub/databases/biomodels/weekly_archives/2012/BioModels-Database-weekly-2012-07-16-sbmls.tar.bz2", "sbml_download")
-getOption("unzip")
-getwd()
-unzip("~Documents/MySummerWork/sbml_download", exdir="sbml")
+
+dest = "sbml_download"
+dest_tar = paste(dest, "tar", sep=".")
+dest_tar_bz2 = paste(dest_tar, "bz2", sep=".")
+
+base_url = "ftp://ftp.ebi.ac.uk/pub/databases/biomodels/weekly_archives/2012/"
+arc = "BioModels-Database-weekly-2012-07-16-sbmls"
+arc_tar_bz2 = paste(arc, ".tar.bz2", sep="")
+download.file(paste(base_url, arc_tar_bz2, sep=""), dest_tar_bz2)
+
+system(paste("bunzip2 ", dest_tar_bz2))
+system(paste("tar xvf ", dest_tar))
+
+file.rename(arc, "sbml_models")
